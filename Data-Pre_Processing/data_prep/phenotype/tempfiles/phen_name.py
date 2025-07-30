9f15fff24e7c2e2b7ed4100f0829c990110ce7f0
Bashir Hussein <71080613+bashirhussein1@users.noreply.github.com>
2025-07-30 14:44:49 +0200

    Delete Data-Pre_Processing/data_prep/phenotype/tempfiles directory

--- Data-Pre_Processing/data_prep/phenotype/tempfiles/phen_name.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-pheno = pd.read_csv('phenotypes_80_final.bed.gz', sep='\t', compression='gzip')
-print(pheno.columns.tolist())
-
-covar = pd.read_csv('covariates_tensorqtl_ready.txt', sep='\t', index_col=0)
-print("Covariate columns:")
-print(covar.columns.tolist())
-
