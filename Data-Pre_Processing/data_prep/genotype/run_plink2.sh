18ef6464d72c0e14df82147ce47353a028fb6ed0
Bashir Hussein <71080613+bashirhussein1@users.noreply.github.com>
2025-07-30 14:41:36 +0200

    Update run_plink2.sh

--- Data-Pre_Processing/data_prep/genotype/run_plink2.sh
+++ Data-Pre_Processing/data_prep/genotype/run_plink2.sh
@@ -6,10 +6,10 @@
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=1
 
-# Input files
-VCF=/home/hbashir1/Data/SNPdata_UroLife_NBCS2/urolife_nbcs2_samen_chr22.dose.vcf.gz
-OUTDIR=/home/hbashir1/nmibc_eqtl_analysis/data_prep/genotype/output_dir
-KEEP=/home/hbashir1/nmibc_eqtl_analysis/data_prep/genotype/samples_to_keep.txt
+# Input files. full path
+VCF=null
+OUTDIR=null
+KEEP=null
 
 # Run PLINK2
 ~/miniconda3/envs/myenv/bin/plink2 --vcf $VCF --output-chr chrM --rm-dup exclude-mismatch --maf 0.01 --keep $KEEP --make-pgen --out $OUTDIR/chr22c_data
