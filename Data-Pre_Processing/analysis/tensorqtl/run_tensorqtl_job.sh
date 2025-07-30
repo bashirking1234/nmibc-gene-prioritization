5453d27a390cc0fc953d73c7534d425e6332036c
Bashir Hussein <71080613+bashirhussein1@users.noreply.github.com>
2025-07-30 14:57:39 +0200

    Update run_tensorqtl_job.sh

--- Data-Pre_Processing/analysis/tensorqtl/run_tensorqtl_job.sh
+++ Data-Pre_Processing/analysis/tensorqtl/run_tensorqtl_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=tensorqtl_run
-#SBATCH --output=/home/hbashir1/nmibc_eqtl_analysis/analysis/tensorqtl/logs/tensorqtl_run.out
-#SBATCH --error=/home/hbashir1/nmibc_eqtl_analysis/analysis/tensorqtl/logs/tensorqtl_run.err
+#SBATCH --output=/tensorqtl_run.out
+#SBATCH --error=/tensorqtl_run.err
 #SBATCH --time=00:20:00          
 #SBATCH --mem=4G
 #SBATCH --cpus-per-task=1
